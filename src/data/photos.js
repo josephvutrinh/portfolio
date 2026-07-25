8eae4f39f639bdece55139f36f019eebc4f79c87
Joseph-Vu Trinh <josephvu.06@gmail.com>
2026-07-25 04:37:39 -0400

    added new picture

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,11 +1,11 @@
 import meow from "../assets/photos/meow.jpeg";
 import july4 from "../assets/photos/july4.jpeg";
 import mattbday from "../assets/photos/mattbday.jpeg";
-import sfskyline from "../assets/photos/sfskyline.jpeg";
+import goldengate from "../assets/photos/goldengate.jpeg";
 
 export const photos = [
     { src: meow, alt: "Earl and Gray", date: "05/02/2026", caption: "My two cats Earl and Gray" },
     { src: july4, alt: "July 4th 2026", date: "07/04/2026", caption: "July 4th 2026" },
     { src: mattbday, alt: "Matt's birthday", date: "07/10/2026", caption: "Matt's birthday" },
-    { src: sfskyline, alt: "San Francisco Skyline", date: "07/11/2026", caption: "San Francisco Skyline" },
+    { src: goldengate, alt: "Golden Gate Bridge", date: "07/11/2026", caption: "Golden Gate Bridge" },
 ];
